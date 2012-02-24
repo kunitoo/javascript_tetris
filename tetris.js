4beedcdd0ad6c836e72805bb6f1337fd46e9bd62
Kunihiko Ito <kuni-ito@esm.co.jp>
2012-02-24 16:23:40 +0900

    chapter 13 ブロックがエリアに溜まるようにする

--- tetris.js
+++ tetris.js
@@ -33,12 +33,43 @@ function paintMatrix(matrix, offsetx, offsety, color) {
     }
     posy = posy + 1;
 }
+function check(map, block, offsetx, offsety) {
+    if (offsetx < 0 || offsety < 0 ||
+        mapHeight < offsety + block.length ||
+        mapWidth < offsetx + block[0].length) {
+        return false;
+    }
+    for (var y = 0; y < block.length; y++) {
+        for (var x = 0; x < block[y].length; x++) {
+            if (block[y][x] && map[y + offsety][x + offsetx]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function mergeMatrix(map, block, offsetx, offsety) {
+    for (var y = 0; y < mapHeight; y++) {
+        for (var x = 0; x < mapWidth; x++) {
+            if (block[y - offsety] && block[y - offsety][x - offsetx]) {
+                map[y][x]++;
+            }
+        }
+    }
+}
 
 function paint() {
     ctx.clearRect(0, 0, 200, 400);
     paintMatrix(map, 0, 0, 'rgb(128, 128, 128)');
     paintMatrix(block, posx, posy, 'rgb(255, 0, 0)');
-    posy = posy + 1;
+    if (check(map, block, posx, posy + 1)) {
+        posy = posy + 1;
+    } else {
+        mergeMatrix(map, block, posx, posy);
+        posx = 0;
+        posy = 0;
+    }
 }
 
 function clean() {
